327ab06f40a094f1a980ef9ff8ad50ac81cc825c
Jason Laster <jason.laster.11@gmail.com>
2014-11-02 08:39:12 -0500

    Update patching to breakup backbone and marionette

--- extension/js/agent/patches/patchBackbone.js
+++ extension/js/agent/patches/patchBackbone.js
@@ -1,36 +1,10 @@
-var patchBackbone = function(onModulesLoaded) {
-  var backboneLoaded = new Promise(function (resolve) {
-    onObjectAndPropertiesSetted(
-      window,
-      'Backbone', ['View', 'Model', 'Router', 'Wreqr', 'Collection', '$'],
-      resolve
-    );
-  });
+this.patchBackbone = function(Backbone) {
+  var Backbone = this.patchedBackbone = Backbone;
 
-  // Because Marionette is assigned to window only when it's ready,
-  // we just need to make sure it's assigned to something
-  // instead of also checking for properties, like with Backbone
-  var marionetteLoaded = new Promise(function (resolve) {
+  debug.log("Backbone detected: ", Backbone);
 
-    var onMarionetteLoaded = function (Marionette) {
-      debug.log('marionette loaded');
-      resolve(Marionette);
-    };
-
-    onceDefined(window, 'Marionette', onMarionetteLoaded);
-
-    backboneLoaded.then(function () {
-      debug.log('backbone loaded');
-      onceDefined(Backbone, 'Marionette', onMarionetteLoaded);
-    });
-  });
-
-  Promise.all([backboneLoaded, marionetteLoaded]).then(function (modules) {
-    debug.log('modules loaded');
-    onModulesLoaded({
-      Backbone: modules[0],
-      Marionette: modules[1]
-    })
-  });
-};
-
+  this.patchBackboneView(Backbone.View)
+  this.patchBackboneModel(Backbone.Model);
+  this.patchBackboneCollection(Backbone.Collection);
+  this.patchBackboneRouter(Backbone.Router);
+}
