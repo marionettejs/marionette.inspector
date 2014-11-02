b360eacddc90f7724b3c079b4942368fa1aebfed
Jason Laster <jason.laster.11@gmail.com>
2014-11-02 08:42:27 -0500

    Add build scripts that were previously gitiginored

--- extension/js/agent/build/build.js
+++ extension/js/agent/build/build.js
@@ -1,4 +1,19 @@
 
+// @include ../../lib/underscore.js
+var _ = this._;
+
+
+// add jQuery if it's not there
+if (_.isUndefined(window.$)) {
+  // @include ../../lib/jquery.js
+}
+
+// add jQuery to window.__agent because Backbone requires it on the root
+this.$ = window.$;
+
+// @include ../../lib/underscore-contrib.js
+
+
 // @include ../../lib/backbone-no-amd.js
 // @include ../../lib/backbone.radio.js
 // @include ../../lib/marionette.js
