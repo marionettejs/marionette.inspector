bb53c889b764163f44647aae73dd7032dc010085
Jason Laster <jason.laster.11@gmail.com>
2014-11-06 16:52:37 -0500

    [agent] Add an Agent start method

--- extension/js/agent/agent.js
+++ extension/js/agent/agent.js
@@ -11,3 +11,13 @@ patchWindow(
   _.bind(this.patchBackbone, this),
   _.bind(this.patchMarionette, this)
 );
+
+
+/* start is a manual way to start the agent if
+ * Backbone and Marionette are not set on the window or
+ * you're not using `define` to package your modules.
+ */
+this.start = function(Backbone, Marionette) {
+  this.patchBackbone(Backbone);
+  this.patchMarionette(Backbone, Marionette);
+};
