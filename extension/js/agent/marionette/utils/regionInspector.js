d0896b2b4274f5473aea40b44c3bfd1331919e2b
Jason Laster <jason.laster.11@gmail.com>
2014-11-11 14:42:40 -0500

    Refactor the regionInspector to be inside a closure & add tests

--- extension/js/agent/marionette/utils/regionInspector.js
+++ extension/js/agent/marionette/utils/regionInspector.js
@@ -1,64 +1,73 @@
 
-this.regionInspector = function(regionTreeRoot, path, shouldSerialize) {
-  shouldSerialize = !!shouldSerialize;
 
-  var regions = this._regionInspector(regionTreeRoot, shouldSerialize)
+this.regionInspector = (function(agent) {
 
-  if (!!path) {
-    regions = objectPath(regions, path, {});
-  }
-
-  //debug.log('region inspector: ', regions);
-  return regions;
-};
-
-this._regionInspector = function (obj, shouldSerialize) {
-
-  if (!obj) {
-    return {};
-  }
-
-  if (obj._regionManager) { // app
-    //debug.log('ri: found app');
-    var subViews = this._subViews(obj, shouldSerialize);
-    return subViews;
-
-  } else if (obj.regionManager) { // layout
-    //debug.log('ri: found layout');
-    var subViews = this._subViews(obj, shouldSerialize);
-    subViews._view =  shouldSerialize ? this.serializeView(obj) :  obj;
-    return subViews;
-
-  } else if (obj.children) { // collection view
-    //debug.log('ri: found collection view');
-
-    var subViews = {};
-    _.each(obj.children._views, function(view, index) {
-      subViews[index] = this._regionInspector(view, shouldSerialize);
-    }, this, subViews, shouldSerialize);
-
-    subViews._view =  shouldSerialize ? this.serializeView(obj) :  obj;
-    return subViews;
-
-  } else { // simple view
-    //debug.log('ri: found simple view');
-
-    return {
-      _view: shouldSerialize ? this.serializeView(obj) :  obj
+  var _regionInspector = function (obj, shouldSerialize) {
+    if (!obj) {
+      return {};
     }
+
+    if (obj._regionManager) { // app
+      //debug.log('ri: found app');
+      var subViews = _subViews(obj, shouldSerialize);
+      return subViews;
+
+    } else if (obj.regionManager) { // layout
+      //debug.log('ri: found layout');
+      var subViews = _subViews(obj, shouldSerialize);
+      subViews._view =  shouldSerialize ? agent.serializeView(obj) :  obj;
+      return subViews;
+
+    } else if (obj.children) { // collection view
+      //debug.log('ri: found collection view');
+
+      var subViews = {};
+      _.each(obj.children._views, function(view, index) {
+        subViews[index] = _regionInspector(view, shouldSerialize);
+      }, this, subViews, shouldSerialize);
+
+      subViews._view =  shouldSerialize ? agent.serializeView(obj) :  obj;
+      return subViews;
+
+    } else { // simple view
+      //debug.log('ri: found simple view');
+
+      return {
+        _view: shouldSerialize ? agent.serializeView(obj) :  obj
+      }
+    }
+  };
+
+  var _subViews = function(obj, shouldSerialize) {
+    var subViews = {};
+    var regions = (obj._regionManager || obj.regionManager)._regions;
+    _.each(regions, function(region, regionName) {
+      var subRegions = _regionInspector(region.currentView, shouldSerialize);
+      subRegions._region = 'region' //region;
+      subViews[regionName] = subRegions;
+    }, this);
+    return subViews;
+  };
+
+
+  /*
+  * regionInspector
+  *
+  * @param {regionTreeRoot} - could be an app or a view
+  * @param {path} - the path is used to get a subtree. e.g. 'header.body' will get only the region tree inside the header body
+  * @param {shouldSerialize} - should the region tree serialize the views
+  */
+
+  return function(regionTreeRoot, path, shouldSerialize) {
+    shouldSerialize = !!shouldSerialize;
+
+    var regions = _regionInspector(regionTreeRoot, shouldSerialize)
+
+    if (!!path) {
+      regions = objectPath(regions, path, {});
+    }
+
+    //debug.log('region inspector: ', regions);
+    return regions;
   }
-};
-
-this._subViews = function(obj, shouldSerialize) {
-  var subViews = {};
-  var regions = (obj._regionManager || obj.regionManager)._regions;
-  _.each(regions, function(region, regionName) {
-    var subRegions = this._regionInspector(region.currentView, shouldSerialize);
-    subRegions._region = 'region' //region;
-    subViews[regionName] = subRegions;
-  }, this);
-  return subViews;
-};
-
-
-// this.objectPath = objectPath;
+}(this))
---
+++ extension/js/test/unit/region-tree.spec.js
@@ -0,0 +1,87 @@
+describe('regionInspector', function() {
+
+  beforeEach(function() {
+    this.setFixtures($("<div class='header'></div><div class='body'></div>"));
+    this.app = new Marionette.Application();
+    this.app.addRegions({
+      header: '.header',
+      body: '.body'
+    });
+  });
+
+  describe('showing a view', function() {
+
+    beforeEach(function() {
+      this.view = new Backbone.View();
+      this.app.header.show(this.view);
+    });
+
+    it('the view is in the region tree', function() {
+      var regionTree = window.regionInspector(this.app);
+      expect(regionTree.header._view).to.deep.equal(this.view);
+    });
+
+    it('the view can be serialized in the tree', function() {
+      var regionTree = window.regionInspector(this.app,'', true);
+      expect(regionTree.header._view.cid).to.deep.equal(this.view.cid);
+    });
+
+    it('can use a subpath', function() {
+      var regionTree = window.regionInspector(this.app,'header', true);
+      expect(regionTree._view.cid).to.deep.equal(this.view.cid);
+    });
+  });
+
+  describe('showing a layout', function() {
+    beforeEach(function() {
+      this.LayoutClass = Marionette.LayoutView.extend({
+        template: _.template('<div class="layout-header"></div><div class="layout-body"></div>'),
+        regions: {
+          header: '.layout-header',
+          body: '.layout-body'
+        }
+      });
+
+      this.layout = new this.LayoutClass();
+      this.app.header.show(this.layout);
+    });
+
+    it('the layout is in the region tree', function() {
+      var regionTree = window.regionInspector(this.app);
+      expect(regionTree.header._view).to.deep.equal(this.layout);
+    });
+
+    it('the layout\'s regions are in the region tree', function() {
+      var regionTree = window.regionInspector(this.app);
+      expect(regionTree.header.header._region ).to.equal("region");
+      expect(regionTree.header.body._region ).to.equal("region");
+    });
+  });
+
+  describe('showing a collection view', function() {
+    beforeEach(function() {
+      this.collection = new Backbone.Collection([{id:1}, {id:2}, {id:3}, {id:4}]);
+      this.CollectionViewClass = Marionette.CollectionView.extend({
+        childView: Backbone.View
+      })
+      this.collectionView = new this.CollectionViewClass({collection: this.collection});
+      this.app.header.show(this.collectionView);
+    });
+
+    it('the collectionView is in the region tree', function() {
+      var regionTree = window.regionInspector(this.app);
+      expect(regionTree.header._view).to.deep.equal(this.collectionView);
+    });
+
+    it('the children views are in tree', function() {
+      var regionTree = window.regionInspector(this.app);
+      var cids = _.keys(this.collectionView.children._views);
+      var cid1 = cids[0];
+      var cid2 = cids[1];
+
+      expect(regionTree.header[cid1]._view).to.deep.equal(this.collectionView.children._views[cid1]);
+      expect(regionTree.header[cid2]._view).to.deep.equal(this.collectionView.children._views[cid2]);
+    });
+
+  })
+})
