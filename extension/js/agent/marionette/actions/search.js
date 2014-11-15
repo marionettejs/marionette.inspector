a3364704b1cdb4a9aa86f5d5812a9b735443e8fe
Jason Laster <jason.laster.11@gmail.com>
2014-11-15 14:12:17 -0500

    [agent] refactor Search

--- extension/js/agent/marionette/actions/search.js
+++ extension/js/agent/marionette/actions/search.js
@@ -1,127 +1,144 @@
 /*
- * search adds a view mouse discovery tool. Once on,
+ * Search adds a view mouse discovery tool. Once on,
  * any view you hover over will get a blue bounding box and on click
  * the view will be saved to window.view.
  *
- * TODOS:
- * 1. when mousing out of a nested region, we need to highlight the outer region
- * 3. show the region path in the bottom left
+ *
  */
 
 this.search = (function(agent) {
-    var search = function(appObserver, app) {
-        var regionTree = appObserver.regionTree();
-        var views = appObserver.viewList();
-
-        var $els = [];
-        _.each(views, function(view) {
-            var $el = view.$el;
-            $els.push($el);
-            $el.attr('data-view-id', view.cid);
-        });
-        var elsViewMap = _.object(_.pluck(views, 'cid'), views);
-        var $elMask;
-        var $currentEl;
-
-        $(document).mouseleave(function() {
-            agent.unhighlightEl();
-        });
-
-        // show the region path in the bottom left
-        // 1. need a regionPathEl map - which i can get by search the regionTree
-        // 2. need to show a div, which i can keep in scope
-
-        _.each($els, function($el) {
-            $el.on('mouseover.regionSearch', function(e) {
-                e.stopPropagation();
-
-                var $current = $(e.currentTarget);
-                $currentEl = $current;
-                var cid = $currentEl.attr('data-view-id');
-                var view = elsViewMap[cid];
-
-                // _.each($els, unhighlightEl);
-                agent.highlightEl($current);
-
-
-                // showViewSummary(view);
-                sendAppComponentReport('search', {
-                  name: 'mouseover',
-                  cid: cid
-                })
-            });
-
-            $el.on('mouseleave.regionSearch', function(e) {
-                e.stopPropagation();
-                var $current = $(e.currentTarget);
-                var cid = $current.attr('data-view-id');
-
-
-                // we used to unhlight views when you left the
-                // the element but that broke when we started showing a higlight mask
-                // also from the user expeience level, it's nice to keep it
-
-                // unhighlightEl($current);
-
-                sendAppComponentReport('search', {
-                  name: 'mouseleave',
-                  cid: cid
-                })
-            });
-
-            $el.on('mousedown.regionSearch', function(e) {
-                e.stopPropagation();
-                var $current = $(e.currentTarget);
-                var cid = $current.attr('data-view-id');
-
-                $elMask = _createElMask($current);
-
-                var view = elsViewMap[$currentEl.attr('data-view-id')];
-                agent.stopSearch(appObserver, app);
-
-                sendAppComponentReport('search', {
-                  name: 'mousedown',
-                  cid: cid
-                }, {immediate: true});
-
-                return false;
-            });
-
-            // move the mousedown handler to the front
-            // would also have to handle subtree mousedown handlers too
-                //var events = $._data($el[0], 'events');
-                //mouseDownHandlers.unshift(mouseDownHandlers.pop());
-
-        }, this);
-    };
 
     /*
-     * creates a mask over the element which captures click events
-     * this is used because we don't want click events to on the view's
-     * element to be captured.
+     * Search constructor starts the search mode
+     *
+     * @param {appObserver} - appObserver used to stop search
+     * @param {app} - used to get the views and stop search
      */
-    var _createElMask = function($el) {
-        var $elMask = $('<div style="position: absolute;">');
-        $elMask.offset($el.offset());
-        $elMask.height($el.outerHeight());
-        $elMask.width($el.outerWidth());
-        $('body').prepend($elMask);
-        $elMask.css('z-index', 10e10);
+    var Search = function(appObserver, app) {
 
-        $elMask.on('click', function() {
-            $elMask.remove();
-        });
+      // this.regionTree = appObserver.regionTree();
+      this.appObserver = appObserver;
+      this.app = app;
+      var views = appObserver.viewList();
+      var $els = _.pluck(views, '$el');
 
-        $elMask.on('mouseup', function() {
-            $elMask.remove();
-        });
+      this.addViewCidAttributes(views);
+      this.viewCidMap = this.createViewCidMap($els, views);
+      this.$clickMask = this.buildClickMask()
+      this.$currentEl = undefined;
 
-        $elMask.on('mouseout', function() {
-            $elMask.remove();
-        });
-        return $elMask;
+      $(document).mouseleave(function() {
+        agent.unhighlightEl();
+      }.bind(this))
+      this.bindElEvents($els);
+
+      console.log('start search', this);
     };
 
-    return search;
+  _.extend(Search.prototype, {
+    /* The viewCidMap is used to look up a view by it's cid.
+     * This is useful because we add a view-cid attribute to each view's element
+     * And we use that to find the view when the element is hovered or clicked on.
+     */
+    createViewCidMap: function(views) {
+      return _.object(_.pluck(views, 'cid'), views);
+    },
+
+    bindElEvents: function($els) {
+      _.each($els, function($el) {
+        $el.on('mouseover.regionSearch', _.bind(this.onMouseOver, this));
+        $el.on('mouseleave.regionSearch', _.bind(this.onMouseLeave, this));
+        $el.on('mousedown.regionSearch', _.bind(this.onMouseDown, this));
+      }, this);
+    },
+
+    onMouseOver: function(e) {
+      e.stopPropagation();
+
+      var $current = $(e.currentTarget);
+      this.$currentEl = $current;
+
+      var cid = this.$currentEl.attr('data-view-id');
+      var view = this.viewCidMap[cid];
+
+      agent.highlightEl($current);
+
+      agent.sendAppComponentReport('search', {
+        name: 'mouseover',
+        cid: cid
+      })
+    },
+
+    onMouseLeave: function(e) {
+      e.stopPropagation();
+      var $current = $(e.currentTarget);
+      var cid = $current.attr('data-view-id');
+
+      agent.sendAppComponentReport('search', {
+        name: 'mouseleave',
+        cid: cid
+      })
+    },
+
+    onMouseDown: function(e) {
+      e.stopPropagation();
+      var $current = $(e.currentTarget);
+      var cid = $current.attr('data-view-id');
+
+      this.placeClickElMask($current);
+
+      var view = this.viewCidMap[cid];
+      agent.stopSearch(this.appObserver, this.app);
+
+      agent.sendAppComponentReport('search', {
+        name: 'mousedown',
+        cid: cid
+      }, {immediate: true});
+
+      return false;
+    },
+
+
+    /*
+     * Creates a mask over the element which captures click events.
+     * This is used because we don't want click events to on the view's
+     * element to be captured.
+     */
+    buildClickMask: function() {
+      var $clickMask = $('<div id="regionSearch-click-mask" style="position: absolute;">');
+      $('body').prepend($clickMask);
+      $clickMask.css('z-index', 10e10);
+
+      return $clickMask;
+    },
+
+    placeClickElMask: function($el) {
+      var $clickMask = this.$clickMask;
+
+      $clickMask.offset($el.offset());
+      $clickMask.height($el.outerHeight());
+      $clickMask.width($el.outerWidth());
+
+      $clickMask.on('click', function() {
+        $clickMask.remove();
+      });
+      $clickMask.on('mouseup', function() {
+        $clickMask.remove();
+      });
+      $clickMask.on('mouseout', function() {
+        $clickMask.remove();
+      });
+    },
+
+    addViewCidAttributes: function(views) {
+      _.each(views, function addCid(view) {
+        view.$el.attr('data-view-id', view.cid);
+      });
+    }
+  })
+
+  return function(appObserver, app) {
+    return new Search(appObserver, app);
+  }
 }(this));
 
