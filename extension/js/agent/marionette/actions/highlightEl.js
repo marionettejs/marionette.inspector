07407d6e1ac59f6f351821d3c9e1ecb0ef8b2348
Jason Laster <jason.laster.11@gmail.com>
2014-11-15 13:26:22 -0500

    Replace highlightMask events with pointer-events none

--- extension/js/agent/marionette/actions/highlightEl.js
+++ extension/js/agent/marionette/actions/highlightEl.js
@@ -1,4 +1,5 @@
 
+// setup the highlight mask
 $(document).ready(function() {
   var $highlightMask = $('<div id="highlightMask" class="marionette-inspector-highlighted-element" style="position: absolute;">');
   $('body').prepend($highlightMask);
@@ -9,12 +10,9 @@ $(document).ready(function() {
 
 
 this.highlightEl = function($el) {
-  if (!$el) {
-    return;
-  }
 
   var $highlightMask = this.$highlightMask;
-  if (!$highlightMask) {
+  if (!$el || !$highlightMask) {
     return;
   }
 
@@ -23,30 +21,8 @@ this.highlightEl = function($el) {
     return;
   }
 
-  $highlightMask.off();
-  $highlightMask.on('mousedown', function(e) {
-    $el.trigger('mousedown');
-    return false;
-  })
-
-  $highlightMask.on('mousemove', function(e) {
-    $highlightMask.css('z-index', -1)
-    var $mouseEl = $(document.elementFromPoint(e.clientX, e.clientY));
-
-    if ($mouseEl != $el) {
-      $mouseEl.trigger('mouseover');
-    }
-
-    $highlightMask.css('z-index', 10e10);
-    return false;
-  });
-
-  $highlightMask.on('mouseleave', function(e) {
-    $el.trigger('mouseleave');
-    return false;
-  });
-
   $highlightMask.css('display', 'block');
+  $highlightMask.css('pointer-events', 'none')
   $highlightMask.offset($el.offset());
   $highlightMask.height($el.outerHeight());
   $highlightMask.width($el.outerWidth());
