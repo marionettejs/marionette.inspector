f4a0b7a6ca6c8dd864fec7c76ef2f4435c9d7e7f
Jason Laster <jason.laster.11@gmail.com>
2014-11-16 21:47:46 -0500

    Add CIDs
    + add cids to knownTypes
    + add cid to serializeObject and insepctValue

--- extension/js/agent/patches/patchMarionetteApplication.js
+++ extension/js/agent/patches/patchMarionetteApplication.js
@@ -9,6 +9,7 @@ this.patchMarionetteApplication = function(MarionetteApplication) {
     var appIndex = registerAppComponent("Application", app);
     if (appIndex === 0) {
       this.patchedApp = app;
+      app.cid = _.uniqueId('c'); // applications dont have cids
       debug.log("Main Marionette application registered");
     }
     patchAppComponentTrigger(app, 'application');
