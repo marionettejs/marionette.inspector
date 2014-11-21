e6a2c4a844f390b4047a2854eb61ad9540ee34e4
Christian Maher <maher.cs@gmail.com>
2014-11-20 21:01:00 -0500

    When patching the define callback, maintain the arity of the callback
    This lets patchDefine work with modules like backbone.projections that
    manage their own dependencies in a require-like fashion.

--- extension/js/agent/patches/patchDefine.js
+++ extension/js/agent/patches/patchDefine.js
@@ -6,73 +6,76 @@ var moduleHasComponents = function (Candidate, components) {
 };
 
 var isBackbone = function(Candidate) {
-    return moduleHasComponents(Candidate, ['View', 'Model', 'Collection', 'Router']);
-}
+  return moduleHasComponents(Candidate, ['View', 'Model', 'Collection', 'Router']);
+};
 
 var isMarionette = function(Candidate) {
-    return moduleHasComponents(Candidate, [
-        'View', 'ItemView', 'CollectionView',
-        'CompositeView', 'Region', 'RegionManager',
-        'Application', 'Module'
-    ]);
-}
+  return moduleHasComponents(Candidate, [
+    'View', 'ItemView', 'CollectionView',
+    'CompositeView', 'Region', 'RegionManager',
+    'Application', 'Module'
+  ]);
+};
 
 // @private
 // Calls the callback passing to it the Backbone object every time it's detected.
 // The function uses multiple methods of detection.
-var patchDefine = function(patchBackbone, patchMarionette) {
-    var loadedModules = {};
-    debug.log('patch define');
+this.patchDefine = function(patchBackbone, patchMarionette) {
+  var agent = this;
+  var loadedModules = {};
+  debug.log('patch define');
 
-    // AMD
-    patchFunctionLater(window, "define", function(originalFunction) {
-      return function() {
-        // debug.log('patched define');
-        // function arguments: (id? : String, dependencies? : Array, factory : Function)
+  // AMD
+  patchFunctionLater(window, "define", function(originalFunction) {
+    return function() {
+      // function arguments: (id? : String, dependencies? : Array, factory : Function)
 
-        // make arguments editable
-        var argumentsArray = Array.prototype.slice.call(arguments);
-        // find the factory function to patch it
-        for (var i=0,l=argumentsArray.length; i<l; i++) {
-            if (typeof argumentsArray[i] == "function") {
-                // factory function found, patch it.
-                // NOTE: in the patcher function, specify the parameters for the
-                // default modules, or in case of a module with no dependencies but
-                // that uses the default modules internally, the original define would see a 0-arity
-                // function and would call it without them (see define() in the AMD API)
-                patchFunction(argumentsArray, i, function(originalFunction) {
-                  return function(require, exports, modules) {
-                    // debug.log('patched argument...');
-                    var module = originalFunction.apply(this, arguments);
+      // make arguments editable
+      var argumentsArray = Array.prototype.slice.call(arguments);
+      // find the factory function to patch it
+      for (var i=0,l=argumentsArray.length; i<l; i++) {
+        if (typeof argumentsArray[i] == "function") {
+          // factory function found, patch it.
+          // NOTE: in the patcher function, specify the parameters for the
+          // default modules, or in case of a module with no dependencies but
+          // that uses the default modules internally, the original define would see a 0-arity
+          // function and would call it without them (see define() in the AMD API)
+          patchFunction(argumentsArray, i, function(originalFunction) {
+            var functionBody = function (bodyArguments) {
+              // debug.log('patched argument...');
+              var module = originalFunction.apply(this, bodyArguments);
 
-                    // check if Backbone has been defined by the factory fuction
-                    // (some factories set "this" to Backbone)
-                    var Candidate = module || this;
+              // check if Backbone has been defined by the factory fuction
+              // (some factories set "this" to Backbone)
+              var Candidate = module || this;
 
-                    if (isBackbone(Candidate)) {
-                      debug.log('Backbone define detected');
-                      loadedModules.Backbone = Candidate;
-                      patchBackbone(Candidate);
+              if (!loadedModules.Backbone && isBackbone(Candidate)) {
+                debug.log('Backbone define detected');
+                loadedModules.Backbone = Candidate;
+                patchBackbone(Candidate);
 
-                      if (isMarionette(Candidate.Marionette)) {
-                        debug.log('Marionette define detected');
-                        loadedModules.Marionette = Candidate.Marionette;
-                        patchMarionette(loadedModules.Backbone, loadedModules.Marionette);
-                      }
-                    }
+                if (isMarionette(Candidate.Marionette)) {
+                  debug.log('Marionette define detected');
+                  loadedModules.Marionette = Candidate.Marionette;
+                  patchMarionette(loadedModules.Backbone, loadedModules.Marionette);
+                }
+              }
 
-                    if (isMarionette(Candidate))  {
-                      debug.log('Marionette define detected');
-                      loadedModules.Marionette = Candidate;
-                      patchMarionette(loadedModules.Backbone, loadedModules.Marionette);
-                    }
+              if (!loadedModules.Marionette && isMarionette(Candidate)) {
+                debug.log('Marionette define detected');
+                loadedModules.Marionette = Candidate;
+                patchMarionette(loadedModules.Backbone, loadedModules.Marionette);
+              }
 
-                    return module;
-                }});
+              return module;
+            };
 
-                break;
-            }
+            return agent.patchDefineCallback(argumentsArray[i], functionBody);
+          });
+
+          break;
         }
-        return originalFunction.apply(this, argumentsArray);
+      }
+      return originalFunction.apply(this, argumentsArray);
     }});
 };
