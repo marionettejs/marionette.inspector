f883ea7bfe3021519af7be7fc2c3914a6a6f75d3
Jason Laster <jason.laster.11@gmail.com>
2014-11-26 11:35:25 -0500

    [inspector] Add ancestor class information
    + Updated ClickableProperties to support propertyKeys like
    constructor.prototype
    + Changed ancestorInfo to get the instance prototype information
    + Started to excusively show ancestor info instead of properties

--- extension/js/test/unit/agent/utils/serializeObject.spec.js
+++ extension/js/test/unit/agent/utils/serializeObject.spec.js
@@ -7,35 +7,37 @@ describe('serializeObject', function() {
       })
 
       this.view = new this.Class();
-
       this.data = window.ancestorInfo(this.view);
     })
 
-    it('serializes Class', function() {
+    it('serializes properties', function() {
       var classData = this.data[0];
       expect(classData.name).to.equal('Class');
       expect(classData.keys).to.deep.equal([
-        "constructor", "_className", "fooFunc"
+        "render", "options", "events", "cid",
+        "$el", "el", "trigger", "ui", "remove",
+        "_listeningTo", "_listenId", "_events"
       ]);
+    });
+
+    it('serializes Class', function() {
+      var classData = this.data[1];
+      expect(classData.name).to.equal('Class');
+      expect(classData.keys).to.deep.equal(["constructor", "_className", "fooFunc"]);
     });
 
     it('serializes ItemView', function() {
-      var classData = this.data[1];
+      var classData = this.data[2];
       expect(classData.name).to.equal('ItemView');
-      expect(classData.keys).to.deep.equal([
-        "constructor", "serializeData", "serializeCollection",
-        "render", "_renderTemplate", "attachElContent",
-        "destroy", "_className"
-      ]);
     });
 
-    it('serializes Marionette View ', function() {
-      var classData = this.data[2];
+    it('serializes Marionette View', function() {
+      var classData = this.data[3];
       expect(classData.name).to.equal('Marionette View');
     });
 
     it('serializes Backbone View', function() {
-      var classData = this.data[3];
+      var classData = this.data[4];
       expect(classData.name).to.equal('Backbone View');
     });
   })
