e928f04282f357135ec00b7eb796465df9426557
Jason Laster <jason.laster.11@gmail.com>
2014-12-09 18:31:46 -0500

    [agent] replace contextData with inspectVale

--- extension/js/agent/marionette/serialize/serializeChannelRadio.js
+++ extension/js/agent/marionette/serialize/serializeChannelRadio.js
@@ -13,7 +13,7 @@ this.serializeChannelRadio = function(channel) {
   _.each(getRadioCommands(channel), function(command, commandName) {
     data.commands[commandName] = {
       name: commandName,
-      context: this.contextData(command.context),
+      context: this.inspectValue(command.context),
       callback: this.inspectValue(command.callback, command.context)
     }
   }, this, data);
@@ -21,7 +21,7 @@ this.serializeChannelRadio = function(channel) {
   _.each(getRadioRequests(channel), function(request, requestName) {
     data.requests[requestName] = {
       name: requestName,
-      context: this.contextData(request.context),
+      context: this.inspectValue(request.context),
       callback: this.inspectValue(request.callback, request.context)
     }
   }, this, data);
@@ -32,7 +32,7 @@ this.serializeChannelRadio = function(channel) {
    _.each(eventHandlers, function(eventHandler) {
     eventHandlerList.push({
       name: eventName,
-      context: this.contextData(eventHandler.context),
+      context: this.inspectValue(eventHandler.context),
       callback: this.inspectValue(eventHandler.callback, eventHandler.context)
     });
    }, this, eventName, eventHandlerList)
@@ -41,18 +41,6 @@ this.serializeChannelRadio = function(channel) {
   return data;
 }
 
-this.contextData = function(context) {
-  if (_.isUndefined(context)) {
-    return {inspect: '', cid: null};
-  }
-
-  var inspect = this.isKnownType(context) ? this.knownType(context).str : '';
-
-  return {
-    inspect: inspect,
-    cid: context.cid
-  };
-}
 
 var getRadioRequests = function(channel) {
   return channel._requests;
