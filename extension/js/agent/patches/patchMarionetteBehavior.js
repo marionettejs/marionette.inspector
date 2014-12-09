8e5715efa5d22d02a688ae11179f9762237d8a3e
Jason Laster <jason.laster.11@gmail.com>
2014-12-09 18:05:51 -0500

    [agent] add behavior cid

--- extension/js/agent/patches/patchMarionetteBehavior.js
+++ extension/js/agent/patches/patchMarionetteBehavior.js
@@ -4,6 +4,14 @@ this.patchMarionetteBehavior = function(MarionetteBehavior) {
     debug.log("Backbone.View detected");
 
     patchBackboneComponent(MarionetteBehavior, bind(function(behavior) { // on new instance
+
+        // collections dont have cids
+        Object.defineProperty(behavior, '__marionette_inspector__cid', {
+          enumerable: false,
+          writable: false,
+          value: _.uniqueId('c')
+        });
+
         var data = {};
         var behaviorIndex = registerAppComponent("Behavior", behavior, data);
     }, this));
