a352c509181edcc437ede7d2ba2032b6a969e293
Jason Laster <jason.laster.11@gmail.com>
2014-12-10 17:38:56 -0500

    [inspector] introduce isTypeNavigable

--- extension/js/inspector/util/isKnownType.js
+++ extension/js/inspector/util/isKnownType.js
@@ -2,8 +2,6 @@ define([
   'util/isViewType',
 ], function(isViewType) {
   return function(object) {
-    // return object.type.match(/backbone/) || object.type.match(/marionette/);
-    var type = object.type || object;
-    return isViewType(object) || type == 'type-backbone-model';
+    return object.type.match(/backbone/) || object.type.match(/marionette/);
   }
 })
