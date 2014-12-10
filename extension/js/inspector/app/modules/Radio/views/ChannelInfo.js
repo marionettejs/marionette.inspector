20f27f82c58961ebb9017d46afa4b2b5ac0cc022
Jason Laster <jason.laster.11@gmail.com>
2014-12-09 19:28:59 -0500

    inspect

--- extension/js/inspector/app/modules/Radio/views/ChannelInfo.js
+++ extension/js/inspector/app/modules/Radio/views/ChannelInfo.js
@@ -2,8 +2,9 @@ define([
   'marionette',
   'util/Radio',
   "text!templates/devTools/radio/info.html",
-  'app/behaviors/SidebarPanes'
-], function(Marionette, Radio, tpl, SidebarPanesBehavior) {
+  'app/behaviors/SidebarPanes',
+  'util/clientInspect'
+], function(Marionette, Radio, tpl, SidebarPanesBehavior, clientInspect) {
 
   return Marionette.ItemView.extend({
 
@@ -12,6 +13,59 @@ define([
     behaviors: {
       sidebarPanes: {
         behaviorClass: SidebarPanesBehavior,
+      }
+    },
+
+    ui: {
+      callback: '[data-property-value-callback]',
+      context: '[data-property-value-context]'
+    },
+
+    events: {
+      'click @ui.context': 'onClickContext',
+      'click @ui.callback': 'onClickCallback'
+    },
+
+    onClickContext: function(e) {
+      var $target = $(e.currentTarget);
+      var $property = $target.closest('li');
+
+      var type = $property.data('property-context-type');
+      var cid = $property.data('property-context-cid');
+      var key = $property.data('property-callback-key');
+
+      this.navigateTo({
+        type: type,
+        cid: cid
+      });
+    },
+
+    onClickCallback: function(e) {
+      var $target = $(e.currentTarget);
+      var $property = $target.closest('li');
+
+      var type = $property.data('property-context-type');
+      var cid = $property.data('property-context-cid');
+      var key = $property.data('property-callback-key');
+
+      function upperCase(str) {
+        return str.charAt(0).toUpperCase() + str.substring(1)
+      }
+
+      type = upperCase(_.last(type.split('-')));
+
+      clientInspect({
+        type: type,
+        cid: cid,
+        path: key
+      });
+    },
+
+    navigateTo: function(object) {
+      if (object && object.type && object.cid) {
+        Radio.command('app', 'navigate:knownObject', {
+          object: object
+        });
       }
     },
 
