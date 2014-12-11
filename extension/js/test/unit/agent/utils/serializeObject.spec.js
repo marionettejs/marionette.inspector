b8a00e046840fa1b85e8955662609fde75be7fa2
Jason Laster <jason.laster.11@gmail.com>
2014-12-11 08:11:14 -0500

    [agent] fix object proerties specificity bug

--- extension/js/test/unit/agent/utils/serializeObject.spec.js
+++ extension/js/test/unit/agent/utils/serializeObject.spec.js
@@ -1,16 +1,30 @@
 describe('serializeObject', function() {
+  beforeEach(function() {
+    this.Class = window.patchedMarionette.ItemView.extend({
+      _className: 'Class',
+      fooFunc: function() {},
+      tagName: 'li'
+    })
+
+    this.view = new this.Class();
+  });
+
+  describe('serializeObjectProperties', function() {
+    beforeEach(function() {
+      this.data = window.serializeObjectProperties(this.view);
+    });
+
+    it('properly serializes overriden properties', function() {
+      expect(this.data.tagName.value).to.equal('li');
+    });
+  });
+
   describe('ancestorInfo', function() {
     beforeEach(function() {
-      this.Class = window.patchedMarionette.ItemView.extend({
-        _className: 'Class',
-        fooFunc: function() {}
-      })
-
-      this.view = new this.Class();
       this.data = window.ancestorInfo(this.view);
     })
 
-    it('serializes properties', function() {
+    it('serializes property keys', function() {
       var classData = this.data[0];
       expect(classData.name).to.equal('Class');
       expect(classData.keys).to.contain(
@@ -23,7 +37,9 @@ describe('serializeObject', function() {
     it('serializes Class', function() {
       var classData = this.data[1];
       expect(classData.name).to.equal('Class');
-      expect(classData.keys).to.deep.equal(["constructor", "_className", "fooFunc"]);
+      expect(classData.keys).to.contain(
+        "constructor", "_className", "fooFunc", "li"
+      );
     });
 
     it('serializes ItemView', function() {
