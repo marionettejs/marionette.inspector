c3e575b81780c09644c9e1ffb42b7776e83594ad
Jason Laster <jason.laster.11@gmail.com>
2014-12-12 14:43:58 -0500

    [agent] add eventIndex to serializeEvents

--- extension/js/agent/marionette/serialize/serializeEvents.js
+++ extension/js/agent/marionette/serialize/serializeEvents.js
@@ -1,18 +1,19 @@
 ;(function (agent) {
 
-  var serializeEvent = function (eventName, event) {
+  var serializeEvent = function (eventName, event, eventIndex) {
     var context = event.context || event.ctx;
     return {
       eventName: eventName,
       callback: agent.inspectValue(event.callback, context),
-      context: agent.inspectValue(context)
+      context: agent.inspectValue(context),
+      eventIndex: eventIndex
     };
   };
 
   agent.serializeEvents = function (events) {
     return _.chain(events).map(function(eventData, eventName) {
-      return _.map(eventData, function(event) {
-        return serializeEvent(eventName, event);
+      return _.map(eventData, function(event, eventIndex) {
+        return serializeEvent(eventName, event, eventIndex);
       })
     }).flatten().value();
   };
