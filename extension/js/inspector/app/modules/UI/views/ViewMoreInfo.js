003d068f88dc93e71215d621bbddc32d18226762
Jason Laster <jason.laster.11@gmail.com>
2014-12-12 11:37:25 -0500

    Add view triggers

--- extension/js/inspector/app/modules/UI/views/ViewMoreInfo.js
+++ extension/js/inspector/app/modules/UI/views/ViewMoreInfo.js
@@ -142,7 +142,7 @@ define([
 
     presentAncestors: function(data, infoItems) {
       var properties = _.omit(data.properties, infoItems,
-              'options', '_events', 'events', 'ui', 'modelEvents', 'collectionEvents', 'el', '$el');
+              'options', '_events', 'events', 'ui', 'triggers', 'modelEvents', 'collectionEvents', 'el', '$el');
       var ancestorInfo = data.ancestorInfo;
 
       var ancestors = [];
