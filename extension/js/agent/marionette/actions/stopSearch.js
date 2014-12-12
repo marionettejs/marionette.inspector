5ef4e22b46aa72dacba4fa40407ba65f847239cb
Jason Laster <jason.laster.11@gmail.com>
2014-12-12 10:56:30 -0500

    fix search

--- extension/js/agent/marionette/actions/stopSearch.js
+++ extension/js/agent/marionette/actions/stopSearch.js
@@ -3,17 +3,14 @@
  * stopSearch stops DevTools.search()
  *
  */
-this.stopSearch = function(appObserver, app) {
-    var regionTree = appObserver.regionTree();
-    var views = appObserver.viewList(regionTree);
-    var $els = $(_.pluck(views, 'el'));
+this.stopSearch = function() {
+    _.each(this.observedElements, function($el) {
+        $el.off('.regionSearch');
+        $el.removeAttr('data-view-id');
+    });
+
+    this.observedElements = [];
 
     $(document).off('mouseleave');
-    _.each($els, function(el) {
-        var $el = $(el);
-        $el.removeAttr('data-view-id');
-        $el.off('.regionSearch');
-    }, this);
-
     this.unhighlightEl();
 };
