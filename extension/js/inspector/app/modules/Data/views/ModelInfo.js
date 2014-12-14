dba79258476bc30b397d4b25e3389f89c122e88d
Jason Laster <jason.laster.11@gmail.com>
2014-12-14 15:59:00 -0500

    [inspector] cleanup presentListeners

--- extension/js/inspector/app/modules/Data/views/ModelInfo.js
+++ extension/js/inspector/app/modules/Data/views/ModelInfo.js
@@ -3,8 +3,9 @@ define([
   'util/Radio',
   "text!templates/devTools/data/info.html",
   'app/behaviors/SidebarPanes',
-  'app/behaviors/ClickableProperties'
-], function(Marionette, Radio, tpl, SidebarPanesBehavior, ClickableProperties) {
+  'app/behaviors/ClickableProperties',
+  'util/presenters/presentListeners'
+], function(Marionette, Radio, tpl, SidebarPanesBehavior, ClickableProperties, presentListeners) {
 
   return Marionette.ItemView.extend({
 
@@ -25,33 +26,13 @@ define([
 
     className: 'sidebar-panel',
 
-    presentListeners: function(listeners) {
-      var data = [];
-
-      _.each(listeners, function(listener) {
-        var callback = listener.callback;
-
-        // _events.show.0.callback
-        var path = ["_events", listener.eventName, listener.eventIndex, "callback"].join(".");
-
-        data.push({
-          context: listeners.context,
-          name: callback.key || callback.inspect,
-          path: path,
-          eventName: listener.eventName
-        });
-      });
-
-      return data;
-    },
-
     serializeData: function() {
       var infoItems = ['cid', 'id', '_pending', '_changing', '_listenerId', 'collection'];
 
       var data = {};
       _.extend(data, this.serializeModel(this.model));
 
-      data.listeners = this.presentListeners(data._events);
+      data.listeners = presentListeners(data._events);
       data.info = _.pick(data.properties, infoItems);
       data.properties = _.omit(data.properties, infoItems,
         'changed', '_previousAttributes', 'attributes', '_events');
