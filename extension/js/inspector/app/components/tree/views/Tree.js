94ae1c7362e31e3e4bbafb8d58af07cc7b45f488
Jason Laster <jason.laster.11@gmail.com>
2014-12-18 17:09:22 -0500

    [inspector] prevent unecessary tree renders

--- extension/js/inspector/app/components/tree/views/Tree.js
+++ extension/js/inspector/app/components/tree/views/Tree.js
@@ -32,40 +32,73 @@ define([
       this.ui = _.extend(Tree.prototype.ui, this.ui || {});
       this.events = _.extend(Tree.prototype.events, this.events || {});
       this.modelEvents = _.extend(Tree.prototype.modelEvents, this.modelEvents || {});
+      this._onCollectionFinishAdding = _.debounce(this._onCollectionFinishAdding, 50);
+
       Backbone.Marionette.CompositeView.prototype.constructor.apply(this, arguments);
+
+      this.i = ++window.i;
+      // console.log('!!! new tree', this.i, this.el);
     },
 
-    initialize: function() {
+    _onCollectionAdd: function() {
+      if (!this.isAdding) {
+        this.isAdding = true;
+        this.startBuffering();
+        this._onCollectionFinishAdding();
+      }
+
+      // console.log('adding collection item', this.cid, this.isBuffering);
+      Marionette.CollectionView.prototype._onCollectionAdd.apply(this, arguments);
+    },
+
+    _onCollectionFinishAdding: function() {
+      if (!this.isAdding) {
+        return;
+      }
+
+      // console.log('finished adding collection', this.cid);
+      this.isAdding = false;
+      this.endBuffering();
     },
 
     onClickToggle: function() {
       this.model.isCollapsed = !this.model.isCollapsed;
-      this.render();
+      this.toggleNode();
       return false;
     },
 
     expandNode: function() {
       this.model.isCollapsed = false;
-      this.render();
+      this.toggleNode();
     },
 
     collapseNode: function() {
       this.model.isCollapsed = true;
-      this.render();
+      this.toggleNode();
+    },
+
+    toggleNode: function() {
+      this.$el.attr('class', this.className());
+      this.$el.children('li').find('.toggle-caret').attr('class', 'toggle-caret glyphicon '+this.chevronClass());
     },
 
     onRender: function() {
+      // console.log('!!! render tree', this.i);
       this.$el.attr('class', this.className());
     },
 
     serializeData: function() {
       var data = Backbone.Marionette.CompositeView.prototype.serializeData.apply(this, this.model);
       data.level = "level-"+this.model.level;
-      data.collapseClass = this.model.isCollapsed ? 'glyphicon-chevron-up' : 'glyphicon-chevron-down'
+      data.collapseClass = this.chevronClass();
       data.hasNodes = this.model.hasNodes();
       data.leafClass = this.model.hasNodes() ? 'is-parent' : 'is-leaf';
 
       return data;
+    },
+
+    chevronClass: function() {
+      return this.model.isCollapsed ? 'glyphicon-chevron-up' : 'glyphicon-chevron-down'
     }
   });
 
