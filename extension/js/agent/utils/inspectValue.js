82e73ddcc106a8a151d864dfe98f1113f6f88ae8
paulfalgout <paul.falgout@roundingwell.com>
2014-12-20 05:09:51 -0600

    [agent] inspectValue refactor
    Agent IIFE
    Recognizes jQuery elements
    A few consistency updates and one perf update. I moved the isKnownType check from the top of inspect() into the array option as that's the only case for it.

--- extension/js/agent/utils/inspectValue.js
+++ extension/js/agent/utils/inspectValue.js
@@ -1,115 +1,120 @@
-/**
- * inspectValue({a: 2, b: 3, c: 4})  //=>
- *   {type:'object', inspect:"{a: 2, b: 3, ... }"}
- *
- */
+(function(Agent) {
 
-this.inspectValue = (function(agent) {
+  /**
+   * inspectValue({a: 2, b: 3, c: 4})  //=>
+   *   {type:'object', inspect:"{a: 2, b: 3, ... }"}
+   *
+   */
+
   var typeOf = function(obj) {
     if (obj instanceof Array) {
-      return 'array'
+      return 'array';
     }
 
     return typeof obj
   }
 
-  var instanceOf = function(obj, Type) {
-    return obj instanceof Type
-  }
-
-  var isType = function(obj, type) {
-    return typeOf(obj) == type;
-  }
-
   var inspect = function(value) {
     var type = typeOf(value) ;
-    if (agent.isKnownType(value)) {
-      return agent.knownTypeString(value);
-    } else if (type === 'undefined') {
-      return "undefined";
-    } else if (type === 'function') {
-      return "function() {";
-    } else if (type === 'array') {
-      if (value.length === 0) { return '[]'; }
-      else if (value.length === 1) { return '[ ' + inspect(value[0]) + ' ]'; }
-      else { return '[ ' + inspect(value[0]) + ', ... ]'; }
-    } else if (value instanceof Error) {
-      return 'Error: ' + value.message;
+
+    if (type === 'undefined') {
+      return 'undefined';
     } else if (value === null) {
       return 'null';
-    } else if(typeOf(value) === 'date') {
+    } else if (type === 'function') {
+      return 'function() {';
+    } else if (type === 'date') {
       return value.toString();
-    } else if(isType(value, 'string') || isType(value, 'number') || isType(value, 'boolean')) {
+    } else if (type === 'string' || type === 'number' || type === 'boolean') {
       return value;
-    } else if (typeof value === 'object') {
+    } else if (value instanceof Error) {
+      return 'Error: ' + value.message;
+    } else if (jQuery && value instanceof jQuery) {
+      var elem = value[0] ? '<'+value[0].tagName.toLowerCase()+'>' : '';
+      return '<jQuery ' + elem + '>';
+    } else if (type === 'array') {
+      if (value.length === 0) { return '[]'; }
+
+      var ret;
+
+      if (Agent.isKnownType(value[0])) {
+        ret = Agent.knownTypeString(value[0]);
+      } else {
+        ret = inspect(value[0]);
+      }
+
+      var suffix = ' ]';
+
+      if(value.length > 1) suffix = ', ... ]';
+
+      return '[ ' + ret + suffix;
+    } else if (type === 'object') {
       // `Ember.inspect` is able to handle this use case,
       // but it is very slow as it loops over all props,
       // so summarize to just first 2 props
       var ret = [], v, count = 0, broken = false;
+
       for (var key in value) {
-        if (key == "length") {
-          continue;
+        if (!value.hasOwnProperty(key)) { continue; }
+
+        if (count++ === 2) {
+          broken = true;
+          break;
         }
 
-        if (value.hasOwnProperty(key)) {
-          if (count++ > 1) {
-            broken = true;
-            break;
-          }
-          v = value[key];
-          if (agent.isKnownType(v)) {v = agent.knownTypeString(v); }
-          else if (v === 'toString') { continue; } // ignore useless items
-          else if (typeOf(v) === 'function') { v = "function() {}"; }
-          else if (typeOf(v) === 'array') { v = '[Array : ' + v.length + ']'; }
-          else if (typeOf(v) === 'object') { v = '[Object]'; }
-          ret.push(key + ": " + v);
-        }
-      }
+        v = value[key];
+
+        if (Agent.isKnownType(v)) { v = Agent.knownTypeString(v); }
+        else if (v === 'toString') { continue; } // ignore useless items
+        else if (typeOf(v) === 'function') { v = 'function() {}'; }
+        else if (typeOf(v) === 'array') { v = '[Array : ' + v.length + ']'; }
+        else if (typeOf(v) === 'object') { v = '[Object]'; }
+
+        ret.push(key + ': ' + v);
+      };
+
       var suffix = ' }';
-      if (broken) {
-        suffix = ' ...}';
-      }
+
+      if(broken) suffix = ' ...}';
+
       return '{ ' + ret.join(', ') + suffix;
+
     } else {
-
-
-      debug.log("serialize: couldn't find the type for ", value);
-      return ''
+      debug.log('serialize: couldn\'t find the type for ', value);
+      return '';
     }
   }
 
 
-  return function(value, object) {
-    var string;
-    var key = agent.findKey(object, value);
+  Agent.inspectValue = function(value, object) {
+    var key = this.findKey(object, value);
 
-    if (agent.isKnownType(value)) {
-      var type = agent.knownType(value);
+    if (this.isKnownType(value)) {
+      var type = this.knownType(value);
       return {
-        type: "type-"+type.name,
+        type: 'type-' + type.name,
         inspect: type.str,
         cid: type.cid,
         key: key
       };
     } else {
       return {
-        type: "type-" + typeOf(value),
+        type: 'type-' + typeOf(value),
         inspect: inspect(value),
         cid: undefined,
         key: key
       };
     }
-  }
+  };
+
+  Agent.inspectObject = function(object) {
+    var value = toJSON(object);
+    return {
+      value: value,
+      inspect: this.inspectValue(object),
+      serialized: this.serializeObject(object),
+      isEmpty: _.isEmpty(value),
+    }
+  };
 
 }(this));
-
-
-this.inspectObject = function(object) {
-  var value = toJSON(object);
-  return {
-    value: value,
-    inspect: this.inspectValue(object),
-    serialized: this.serializeObject(object),
-    isEmpty: _.isEmpty(value),
-  }
-}
