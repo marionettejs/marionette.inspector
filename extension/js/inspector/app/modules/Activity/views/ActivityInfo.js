deb7d72ddb9d6a814608cfa5d8dd1c3c21b62db9
Jason Laster <jason.laster.11@gmail.com>
2014-12-20 18:03:10 -0500

    Add better precision duration

--- extension/js/inspector/app/modules/Activity/views/ActivityInfo.js
+++ extension/js/inspector/app/modules/Activity/views/ActivityInfo.js
@@ -19,10 +19,16 @@ define([
       }
     },
 
+    presentDuration: function() {
+      var duration = this.model.get('endTime') - this.model.get('startTime');
+      return Math.round(duration*1000)/1000;
+    },
+
     serializeData: function() {
       var data = ActivityInfo.__super__.serializeData.apply(this, arguments);
-      data.duration = this.model.get('endTime') - this.model.get('startTime');
+      data.duration = this.presentDuration();
       data.hasArgs = this.model.get('args') && this.model.get('args').length;
+
       return data;
     }
   });
