975a0150fd6630e7ddaff21af896d60331ab7a3a
paulfalgout <paul.falgout@roundingwell.com>
2014-12-20 05:15:55 -0600

    Agent IIFE for utils
    First step towards getting agent off of `this` and onto it's own object.

--- extension/js/agent/utils/isNativeFunction.js
+++ extension/js/agent/utils/isNativeFunction.js
@@ -1,4 +1,6 @@
-this.isNativeFunction = function(func) {
-  var funcStr = func.toString()
-  return funcStr.match(/native code/) || funcStr.match(/function bound()/);
-}
+(function(Agent) {
+  Agent.isNativeFunction = function(func) {
+    var funcStr = func.toString()
+    return funcStr.match(/native code/) || funcStr.match(/function bound()/);
+  };
+}(this));
