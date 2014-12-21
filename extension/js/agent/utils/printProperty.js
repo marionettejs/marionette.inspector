1026a6fd42105b11b8364ace0fb2a093383de42e
Jason Laster <jason.laster.11@gmail.com>
2014-12-21 09:55:43 -0500

    re-add two removed utils

---
+++ extension/js/agent/utils/printProperty.js
@@ -0,0 +1,14 @@
+;(function(Agent) {
+
+  Agent.tempId = 0;
+
+
+  Agent.printProperty = function(prop) {
+    var tempVar = 'mn' + ++Agent.tempId;
+
+    window[tempVar] = prop;
+    console.log(tempVar + ' = ', prop);
+  };
+
+
+}(this));
