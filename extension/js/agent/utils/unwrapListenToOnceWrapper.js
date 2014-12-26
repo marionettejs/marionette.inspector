61f591edd916a343bc2efcd7b0f008e3a1d2382c
paulfalgout <paul.falgout@roundingwell.com>
2014-12-26 01:45:07 -0600

    Simply unwrapper
    This solves minified or newer versions of underscore being broken.
    
    And it works with recent versions of backbone

--- extension/js/agent/utils/unwrapListenToOnceWrapper.js
+++ extension/js/agent/utils/unwrapListenToOnceWrapper.js
@@ -1,34 +1,7 @@
 (function(Agent) {
 
-  var underscoreListenOnceFuncString = function () {
-        if (ran) return memo;
-        ran = true;
-        memo = func.apply(this, arguments);
-        func = null;
-        return memo;
-      }.toString();
-
-  var lodashListenOnceFuncString = function() {
-          if (ran) {
-            return result;
-          }
-          ran = true;
-          result = func.apply(this, arguments);
-
-          // clear the `func` variable so the function may be garbage collected
-          func = null;
-          return result;
-        }.toString();
-
   Agent.unwrapListenToOnceWrapper = function(func) {
-    var funcString = func.toString();
-    if (underscoreListenOnceFuncString == funcString
-      || lodashListenOnceFuncString == funcString) {
-
-      return func._callback;
-    }
-
-    return func;
+    return func._callback || func;
   }
 
 })(this);
