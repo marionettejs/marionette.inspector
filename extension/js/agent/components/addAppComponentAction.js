44e5e8fb6f3870a75371851a90a12aabee21a228
paulfalgout <paul.falgout@roundingwell.com>
2015-01-05 04:34:10 -0600

    Compontents IIFE and refactor
    Put AppCompontentInfo things together
    Refactor getAppViewInfoFromElement to use _.find

--- extension/js/agent/components/addAppComponentAction.js
+++ extension/js/agent/components/addAppComponentAction.js
@@ -1,32 +1,33 @@
+;(function(Agent) {
 
-// @private
-// Restituisce l'indice dell'azione aggiunta.
-var addAppComponentAction = _.bind(function(appComponent, appComponentAction) {
-    var appComponentInfo = this.getAppComponentInfo(appComponent);
+  // @private
+  // Restituisce l'indice dell'azione aggiunta.
+  Agent.addAppComponentAction = _.bind(function(appComponent, appComponentAction) {
+      var appComponentInfo = Agent.getAppComponentInfo(appComponent);
 
-    if (!appComponentInfo) {
-        debug.log('addAppComponentAction could not find action');
-        return;
-    }
+      if (!appComponentInfo) {
+          debug.log('addAppComponentAction could not find action');
+          return;
+      }
 
-    appComponentInfo.actions.push(appComponentAction);
-    var actionIndex = appComponentInfo.actions.length-1;
+      appComponentInfo.actions.push(appComponentAction);
+      var actionIndex = appComponentInfo.actions.length - 1;
 
-    // invia un report riguardante la nuova azione
-    var reportName = appComponentInfo.category+":"+appComponentAction.type;
-    var reportData = {
-        componentActionIndex: actionIndex,
-        type: appComponentAction.type,
-        name: appComponentAction.name,
-        data: _.extend(appComponentAction.data || {}, {
-            cid: appComponent.cid, // might not exist
-        })
-    };
+      // invia un report riguardante la nuova azione
+      var reportName = appComponentInfo.category + ':' + appComponentAction.type;
+      var reportData = {
+          componentActionIndex: actionIndex,
+          type: appComponentAction.type,
+          name: appComponentAction.name,
+          data: _.extend(appComponentAction.data || {}, {
+              cid: appComponent.cid, // might not exist
+          })
+      };
 
-    sendAppComponentReport(reportName, reportData);
-    // debug.log("action: ", reportName);
+      Agent.sendAppComponentReport(reportName, reportData);
+      // debug.log("action: ", reportName);
 
-    return actionIndex;
-}, this);
+      return actionIndex;
+  }, Agent);
 
-this.addAppComponentAction = addAppComponentAction;
+}(this));
--- extension/js/agent/components/registerAppComponent.js
+++ extension/js/agent/components/registerAppComponent.js
@@ -1,38 +1,46 @@
+;(function(Agent) {
 
-// @private
-// Aggiunge il componente dell'app passato a quelli conosciuti creando l'oggetto con le info
-// e inviando un report all'esterno per informare il resto del mondo.
-// Restituisce l'indice del componente.
-var registerAppComponent = _.bind(function(appComponentCategory, appComponent, componentData) {
-    // calcola l'indice del nuovo componente
-    if (lastAppComponentsIndex[appComponentCategory] == null) {
-      lastAppComponentsIndex[appComponentCategory] = -1;
-    }
-    var appComponentIndex = ++lastAppComponentsIndex[appComponentCategory];
+  // @private
+  // All'atto dell'istanziazione di un componente, l'agent gli assegna
+  // un indice che lo identifica all'interno dei vari array
+  // riguardanti i componenti di quella categoria.
+  // Tale indice viene calcolato incrementando quello dell'ultimo componente
+  // della propria categoria.
+  var lastAppComponentsIndex = {};
 
-    setAppComponentInfo(appComponent, new AppComponentInfo(
-      appComponentCategory,
-      appComponentIndex,
-      appComponent
-    ));
+  // @private
+  // Aggiunge il componente dell'app passato a quelli conosciuti creando l'oggetto con le info
+  // e inviando un report all'esterno per informare il resto del mondo.
+  // Restituisce l'indice del componente.
+  Agent.registerAppComponent = function(appComponentCategory, appComponent, componentData) {
+      // calcola l'indice del nuovo componente
+      if (lastAppComponentsIndex[appComponentCategory] === undefined) {
+        lastAppComponentsIndex[appComponentCategory] = -1;
+      }
+      var appComponentIndex = ++lastAppComponentsIndex[appComponentCategory];
 
+      Agent.setAppComponentInfo(appComponent, new Agent.AppComponentInfo(
+        appComponentCategory,
+        appComponentIndex,
+        appComponent
+      ));
 
-    componentData = componentData || {};
+      componentData = componentData || {};
 
-    // invia un report riguardante il nuovo componente dell'app
-    var reportName = appComponentCategory+":new";
-    var reportData = {
-      componentIndex: appComponentIndex,
-      type: 'new',
-      name: appComponentCategory,
-      data: componentData
-    };
+      // invia un report riguardante il nuovo componente dell'app
+      var reportName = appComponentCategory + ':new';
+      var reportData = {
+        componentIndex: appComponentIndex,
+        type: 'new',
+        name: appComponentCategory,
+        data: componentData
+      };
 
-    sendAppComponentReport(reportName, reportData);
+      Agent.sendAppComponentReport(reportName, reportData);
 
-    debug.log("registerAppComponent " + reportName);
+      debug.log('registerAppComponent ' + reportName);
 
-    return appComponentIndex;
-}, this);
+      return appComponentIndex;
+  };
 
-this.registerAppComponent = registerAppComponent;
+}(this));
