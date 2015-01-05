f94070ada198db91886c6f676c53936c3cba04e8
paulfalgout <paul.falgout@roundingwell.com>
2015-01-05 04:34:10 -0600

    Compontents IIFE and refactor
    Put AppCompontentInfo things together
    Refactor getAppViewInfoFromElement to use _.find

--- extension/js/agent/components/util/sendAppComponentReport.js
+++ extension/js/agent/components/util/sendAppComponentReport.js
@@ -1,21 +1,19 @@
+;(function(Agent){
 
-// @private
-// Note: name is prefixed by "agent:" and can't contain spaces
-//       (because it's transformed in a Backbone event in the Panel)
-var sendAppComponentReport = function(name, report, options) {
-    report = report || {};
-    options = options || {};
+  // @private
+  // Note: name is prefixed by "agent:" and can't contain spaces
+  //       (because it's transformed in a Backbone event in the Panel)
+  Agent.sendAppComponentReport = function(name, report, options) {
+      report = report || {};
+      options = options || {};
 
-    // the timestamp is tipicaly used by the panel to exclude old reports
-    report.timestamp = new Date().getTime();
+      // the timestamp is tipicaly used by the panel to exclude old reports
+      report.timestamp = new Date().getTime();
 
-    sendMessage({
-        name: "agent:"+name,
-        data: report
-    }, options);
-};
+      Agent.sendMessage({
+          name: 'agent:' + name,
+          data: report
+      }, options);
+  };
 
-//TODO: remove when components have Agent IIFE
-sendMessage = this.sendMessage;
-
-this.sendAppComponentReport = sendAppComponentReport;
+}(this));
---
+++ extension/js/agent/patches/util/addCidToComponent.js
@@ -0,0 +1,14 @@
+;(function(Agent) {
+
+  /**
+   Some backbone and marionette objects don't have cids (gasp)
+   When that happens we go in and add this property __marionette_inspector__cid
+   with a stupid long and obscure name. We use Object.defineProperty so that we
+   can effectively hide the property with enumerable and writable set to false.
+  */
+
+  Agent.addCidToComponent = function(object) {
+    Agent.setHiddenProperty(object, 'cid',  _.uniqueId('c'));
+  }
+
+}(this));
