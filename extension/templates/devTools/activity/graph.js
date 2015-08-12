dd40c9d39e2fc39eb742fbe1c774ea873c616de8
Melanie Gin <melaniegin1@gmail.com>
2015-08-12 12:07:18 -0700

    update ActivityGraph for mouseover, work in progress

--- extension/templates/devTools/activity/graph.js
+++ extension/templates/devTools/activity/graph.js
@@ -36,6 +36,7 @@ define(['d3', 'util/Radio'], function(d3, Radio) {
 
       var activityGraph = d3.select('div.activity-graph');
 
+      var margin = 10;
       var width = +activityGraph.style('width').split('px')[0];
       var height = 100;
 
@@ -47,30 +48,44 @@ define(['d3', 'util/Radio'], function(d3, Radio) {
         .domain([0, 100])
         .range([0, height]);
 
-      var color = d3.scale.category20c();
+      var color = d3.scale.category20();
 
       var svg = activityGraph.append("svg")
-        .attr("width", width)
-        .attr("height", height);
+        .attr("width", width + (2 * margin))
+        .attr("height", height + (2 * margin));
 
-      rect = svg.selectAll("rect")
+      rect = svg.selectAll('rect')
           .data(formattedData)
         .enter().append("rect")
-          .attr("x", function(d) { return d.attributes.position.x - startX; })
-          .attr("y", function(d) { return d.attributes.position.y; })
-          .attr("width", function(d) { return 500 * d.attributes.position.dx; })
+          .attr("x", function(d) { return x(d.attributes.position.x); })
+          .attr("y", function(d) { return y(d.attributes.position.y); })
+          .attr("width", function(d) { return 100 * d.attributes.position.dx; })
           .attr("height", function(d) { return d.attributes.position.dy; })
           .attr("fill", function(d) { return color(d.attributes.eventName); })
-          .on("click", onShowInfo);
+          .attr("id", function(d) { return "id" + d.attributes.eventId; })
+          .on("click", onClick)
+          .on("mouseover", onMouseover);
 
-      function onShowInfo(d) {
+      function onClick(d) {
 
         var eventId = d.attributes.eventId;
-
         Radio.command('activity', 'click:graph', eventId);
 
       }
 
+      // function onMouseover(event) {
+      //   console.log("event: ", event);
+      //   d3.select("text").remove();
+
+      //   d3.select("rect#id"+ event.attributes.eventId).append('text')
+      //   .attr('x', function(d) { return x(d.attributes.position.x); })
+      //   .attr('y', function(d) { return y(d.attributes.position.y); })
+      //   .text(function(d) {
+      //     console.log("d.attributes.eventName: ", d.attributes.eventName);
+      //     return d.attributes.eventName;
+      //   });
+
+      // }
     }
   };
 
