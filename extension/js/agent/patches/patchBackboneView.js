599320b6e64266b87e19329ca0091a67ff0725a9
Luiz Américo <luizamericop@gmail.com>
2017-06-30 06:36:50 -0300

    Reduce the number of times a view is serialized

--- extension/js/agent/patches/patchBackboneView.js
+++ extension/js/agent/patches/patchBackboneView.js
@@ -1,6 +1,7 @@
 ;(function(Agent){
 
-  var patchViewChanges = _.debounce(function(view, prop, action, difference, oldValue) {
+  var patchViewChanges = function(view, prop, action, difference, oldValue) {
+    debug.log('view:change', view.cid);
     Agent.lazyWorker.push({
       context: Agent,
       args: [view],
@@ -11,49 +12,55 @@
         });
       }
     });
-  }, 200);
+  };
+
+  var onNewViewInstance = function(view) { // on new instance
+    debug.log('new view instance:', view.cid);
+    // flag to not trigger view:change when is registering
+    var isRegisteringView = true;
+    var viewChanged = _.debounce(_.partial(patchViewChanges, view), 0);
+    Agent.lazyWorker.push({
+      context: Agent,
+      args: [view],
+      callback: function(view) {
+        // registra il nuovo componente dell'app
+        var data = Agent.serializeView(view);
+        Agent.registerAppComponent('View', view, data);
+      }
+    });
+
+    // set class name if we can
+    if (view._requirePath) {
+      view._className = _.last(view._requirePath.split('/'));
+    }
+
+    // Patcha i metodi del componente dell'app
+    Agent.patchAppComponentTrigger(view, 'view');
+
+    Agent.onDefined(view, 'ui', function() {
+      Agent.onChange(view.ui, viewChanged);
+      if (!isRegisteringView) viewChanged();
+    });
+
+    Agent.onDefined(view, '_events', function() {
+      Agent.onChange(view._events, viewChanged);
+      if (!isRegisteringView) viewChanged();
+    });
+
+    Agent.onDefined(view, '_listeningTo', function() {
+      Agent.onChange(view._listeningTo, viewChanged);
+      if (!isRegisteringView) viewChanged();
+    });
+
+    isRegisteringView = false;
+  };
 
   // @private
   Agent.patchBackboneView = function(BackboneView) {
     debug.log('Backbone.View detected');
 
-    Agent.patchBackboneComponent(BackboneView, function(view) { // on new instance
-      Agent.lazyWorker.push({
-        context: Agent,
-        args: [view],
-        callback: function(view) {
-          // registra il nuovo componente dell'app
-          var data = Agent.serializeView(view)
-          Agent.registerAppComponent('View', view, data);
-          Agent.sendAppComponentReport('view:change', {
-            data: data,
-            cid: view.cid
-          });
-        }
-      });
-
-      // set class name if we can
-      if (view._requirePath) {
-        view._className = _.last(view._requirePath.split('/'));
-      }
-
-      // Patcha i metodi del componente dell'app
-      Agent.patchAppComponentTrigger(view, 'view');
-
-      Agent.onDefined(view, 'ui', function() {
-        Agent.onChange(view.ui, _.partial(patchViewChanges, view));
-        patchViewChanges(view);
-      });
-
-      Agent.onDefined(view, '_events', function() {
-        Agent.onChange(view._events, _.partial(patchViewChanges, view));
-        patchViewChanges(view);
-      });
-
-      Agent.onDefined(view, '_listeningTo', function() {
-        Agent.onChange(view._listeningTo, _.partial(patchViewChanges, view));
-        patchViewChanges(view);
-      });
+    Agent.patchBackboneComponent(BackboneView, function (view) {
+      _.defer(onNewViewInstance, view);
     });
   };
 
